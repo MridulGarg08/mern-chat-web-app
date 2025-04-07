6493274c4893ace37bd4eb2fbbbc98239b26c3ee
Mridul Garg <mridulgarg8888@gmail.com>
2025-04-07 16:14:14 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express" ;
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,22 +9,29 @@ import userRoutes from "./routes/user.Routes.js"
 
 import connectMongo from "./db/connectToMongoDB.js"
 import { app, server } from "./socket/socket.js";
-// import authroutes from "./routes/auth.routes.js"
+
 
 dotenv.config();
+const __dirname = path.resolve();
 const port=process.env.PORT||5000;
 app.use(express.json())
 app.use(cookieParser());
 
-app.get("/",(req,res)=>{
-    // root route "http://localhost:5000/"
-    res.send("Hello World");
-    console.log("Hello World");
-})
+// app.get("/",(req,res)=>{
+//     // root route "http://localhost:5000/"
+//     res.send("Hello World");
+//     console.log("Hello World");
+// })
 
 app.use("/api/auth", authroutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(port, () => {
     connectMongo();
